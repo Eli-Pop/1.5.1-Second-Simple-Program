6a9ee23cb9d496690d209de0ac8c250faa1fc8d3
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-17 21:43:09 -0500

    Add files via upload
    Simple C++ program show casing declarations of variables and printing in line texts.
    Output will display via command prompt.

---
+++ 1.5.1 Second Simple Program.cpp
@@ -0,0 +1,17 @@
+// Using a variable.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length; // this declares a variable
+	length = 7; // this assigns 7 to length
+
+	cout << "The length is ";
+	cout << length << "\n"; // This displays 7
+
+	system("pause");
+	return 0;
+
+}
